97ce8205e00c63c9243370ea85efe48d7e8bfebd
Bryant <tangyu990628@gmail.com>
2023-11-30 12:03:21 +0100

    LGR

--- DKT/KnowledgeTracing/evaluation/run.py
+++ DKT/KnowledgeTracing/evaluation/run.py
@@ -16,23 +16,23 @@ import torch.optim as optim
 from evaluation import eval
 import wandb
 
-if torch.cuda.is_available():
-    # os.environ["CUDA_VISIBLE_DEVICES"] = cuda
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
-# device = torch.device('cpu')
+# if torch.cuda.is_available():
+#     # os.environ["CUDA_VISIBLE_DEVICES"] = cuda
+#     device = torch.device('cuda')
+# else:
+#     device = torch.device('cpu')
+device = torch.device('cpu')
 # Initialize Weights and Biases with your API key and project name
-wandb.init(
-    project="DKT-trial 1",
-    name="df1_LSTM",
+# wandb.init(
+#     project="DKT-trial 1",
+#     name="df1_LSTM",
 
-)
+# )
 
 print('Dataset: ' + C.DATASET + ', Learning Rate: ' + str(C.LR) + '\n')
 
 # model = DKT(C.INPUT, C.HIDDEN, C.LAYERS, C.OUTPUT).to(device)
-model = LGR(C.INPUT, C.OUTPUT).to(device)
+model = LGR(C.INPUT, C.HIDDEN, C.OUTPUT).to(device)
 optimizer_adam = optim.Adam(model.parameters(), lr=C.LR)
 optimizer_adgd = optim.Adagrad(model.parameters(),lr=C.LR)
 # optimizer_adgd.state = {key: value.to(device) for key, value in optimizer_adgd.state.items()}
@@ -51,16 +51,16 @@ for epoch in range(C.EPOCH):
     if val_auc>best_auc:
         best_auc=val_auc
         torch.save(model.state_dict(), 'df1_LSTM.pth')
-    wandb.log({"train_loss": train_loss}, step = epoch)
-    wandb.log({"val_loss": val_loss}, step=epoch)
-    wandb.log({"train_auc": train_auc}, step = epoch)
-    wandb.log({"val_auc": val_auc}, step = epoch)
+#     wandb.log({"train_loss": train_loss}, step = epoch)
+#     wandb.log({"val_loss": val_loss}, step=epoch)
+#     wandb.log({"train_auc": train_auc}, step = epoch)
+#     wandb.log({"val_auc": val_auc}, step = epoch)
     
-    # wandb.log({"train_f1": train_f1}, step=epoch)
-    # wandb.log({"train_recall": train_recall}, step=epoch)
-    # wandb.log({"train_precision": train_precision}, step=epoch)
+#     # wandb.log({"train_f1": train_f1}, step=epoch)
+#     # wandb.log({"train_recall": train_recall}, step=epoch)
+#     # wandb.log({"train_precision": train_precision}, step=epoch)
     
-    # wandb.log({"val_f1": val_f1}, step=epoch)
-    # wandb.log({"val_recall": val_recall}, step=epoch)
-    # wandb.log({"val_precision": val_precision}, step=epoch)
-wandb.finish()
+#     # wandb.log({"val_f1": val_f1}, step=epoch)
+#     # wandb.log({"val_recall": val_recall}, step=epoch)
+#     # wandb.log({"val_precision": val_precision}, step=epoch)
+# wandb.finish()
--- DKT/KnowledgeTracing/model/LGR.py
+++ DKT/KnowledgeTracing/model/LGR.py
@@ -6,15 +6,23 @@ from torch.autograd import Variable
 from sklearn.model_selection import train_test_split
 
 class LGR(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, output_dim):
         super(LGR, self).__init__()
         self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
         self.output_dim = output_dim
-        self.fc = nn.Linear(self.input_dim, self.output_dim)
+        self.tanh = nn.Tanh()
+        self.hidden_layer = nn.Linear(input_dim, hidden_dim)
+        self.output_layer = nn.Linear(hidden_dim, output_dim)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        out = self.fc(x)
-        res = self.sigmoid(out)
+        # h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim)).to(x.device)
+        # out,hn = self.rnn(x, h0)
+        hidden = self.tanh(self.hidden_layer(x))
+        
+        # Apply sigmoid activation for binary classification
+        res = self.sigmoid(self.output_layer(hidden))
+        
         return res
 
